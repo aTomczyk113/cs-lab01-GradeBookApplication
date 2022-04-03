abfd5cd46f07375be90da974904a2a6304a59914
Arkadiusz Tomczyk <arkadiusz.tomczyk@microsoft.wsei.edu.pl>
2022-04-03 16:27:05 +0200

    Add Type property

---
+++ GradeBook/GradeBooks/StandardGradeBook.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GradeBook.GradeBooks
+{
+    public class StandardGradeBook
+    {
+         public StandardGradeBook(string name)
+        {
+
+        }
+    }
+}
