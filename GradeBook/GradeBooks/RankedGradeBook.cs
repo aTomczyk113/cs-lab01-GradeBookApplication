4ddeb211ab169ae944bc13254532fdf8cf4de359
Arkadiusz Tomczyk <arkadiusz.tomczyk@microsoft.wsei.edu.pl>
2022-04-03 16:53:48 +0200

    Override RankedGradeBook's GetLetterGrade method

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -11,6 +11,37 @@ namespace GradeBook.GradeBooks
         {
             Type = GradeBookType.Ranked;
         }
+        public override char GetLetterGrade(double averageGrade)
+        {
+            if (Students.Count < 5)
+            {
+                throw new InvalidOperationException();
+            }
+            int x = (int)Math.Ceiling(Students.Count * 0.20);
+            int l = 0;
 
+            for (int i = 0; i < Students.Count; i++)
+            {
+                double m = 0;
+                for (int j = 0; j < Students[i].Grades.Count; j++)
+                {
+                    m = +Students[i].Grades[j];
+                }
+                if (averageGrade >= m / Students[i].Grades.Count)
+                {
+                    l++;
+                }
+            }
+            if (l <= (int)Math.Ceiling(Students.Count * 0.20))
+                return 'F';
+            else if (l <= (int)Math.Ceiling(Students.Count * 0.40))
+                return 'D';
+            else if (l <= (int)Math.Ceiling(Students.Count * 0.60))
+                return 'C';
+            else if (l <= (int)Math.Ceiling(Students.Count * 0.80))
+                return 'B';
+            else
+                return 'A';
+        }
     }
 }
